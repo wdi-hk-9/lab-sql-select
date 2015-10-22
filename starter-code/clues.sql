472f11632dbc77e22073abd40ed6ce183049ad95
Jacky Lee <jackywhlee@gmail.com>
2015-10-23 01:51:44 +0800

    decided to skip to clue #8 since that was enough to find the answer

--- starter-code/clues.sql
+++ starter-code/clues.sql
@@ -1,18 +1,26 @@
 -- Clue #1: We recently got word that someone fitting Carmen Sandiego's description has been
 -- traveling through Southern Europe. She's most likely traveling someplace where she won't be noticed,
 -- so find the least populated country in Southern Europe, and we'll start looking for her there.
-
+ SELECT name, population FROM country WHERE region = 'Southern Europe' ORDER BY population;
+ = Holy See (Vatican City State)
 
 
 -- Clue #2: Now that we're here, we have insight that Carmen was seen attending language classes in
 -- this country's officially recognized language. Check our databases and find out what language is
 -- spoken in this country, so we can call in a translator to work with you.
+SELECT code1 FROM country where name LIKE '%Holy%';
+= VAT
+SELECT language FROM countrylanguage WHERE countrycode='VAT';
+= Italian
 
 
 
 -- Clue #3: We have new news on the classes Carmen attended – our gumshoes tell us she's moved on
 -- to a different country, a country where people speak only the language she was learning. Find out which
 --  nearby country speaks nothing but that language.
+SELECT countrycode FROM countrylanguage WHERE language='Italian' AND  isofficial='t';
+= ITA, SMR, VAT, CHE
+
 
 
 
@@ -52,6 +60,8 @@
 
 -- We're counting on you, gumshoe. Find out where she's headed, send us the info, and we'll be sure to meet her at the gates with bells on.
 
+ SELECT name FROM city WHERE population='91084';
 
 
--- She's in ____________________________!
+
+-- She's in  Santa Monica!
